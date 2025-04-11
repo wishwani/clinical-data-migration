ef78aa0284ecfe13e2ab19a818d1bc336fe280fc
RATHWATHTHAGE Randika Wishwani <randika.wishwani@gmail.com>
2025-04-11 10:27:31 +0200

    remove unnessary files and add .bat for task schedular

---
+++ run_data_migration.bat
@@ -0,0 +1,5 @@
+@echo off
+set PYTHON_PATH="C:\Users\Randika\AppData\Local\Programs\Python\Python313\python.exe"
+set SCRIPT_PATH="C:\Users\Randika\MedDataSync\python_integration.py"
+
+%PYTHON_PATH% %SCRIPT_PATH%
